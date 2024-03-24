4e77209e2f148552b685b0184d37ca8fa9f78f18
Kamatis <62044147+recillajake@users.noreply.github.com>
2024-03-24 13:39:56 +0800

    added logo

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def index():
 
 @app.route('/login_process')
 def login_process():
-    render_template('login_process.html')
+    return render_template('login_process.html')
 
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    render_template('home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
