88514ca16a8f391b32707d65216dc7ac5ab05d49
khareen1100 <marsanjaysaladaga@gmail.com>
2024-03-24 12:04:02 +0800

    first commit
    making flask app

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World! This is my Flask app.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
