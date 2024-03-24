1ac1f4ce92355bb56c442710db44e61227ee3f90
Kamatis <62044147+recillajake@users.noreply.github.com>
2024-03-24 13:16:40 +0800

    added logo and login
    uwu

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask,render_template,requests
+from flask import Flask,render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World! This is my Flask app.'
+    return render_template('index.html')
+
+@app.route('/login_process')
+def login_process():
+    render_template('login_process.html')
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
